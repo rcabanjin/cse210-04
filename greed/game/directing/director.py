8bd73f94972889cf37c471120ff4ba2e4577f503
dic09005 <dic09005@byui.edu>
2022-11-04 12:39:42 -0600

    director file

--- greed/game/directing/director.py
+++ greed/game/directing/director.py
@@ -2,7 +2,6 @@ class Director:
     """A person who directs the game. 
     
     The responsibility of a Director is to control the sequence of play.
-
     Attributes:
         _keyboard_service (KeyboardService): For getting directional input.
         _video_service (VideoService): For providing video output.
@@ -17,13 +16,14 @@ class Director:
         """
         self._keyboard_service = keyboard_service
         self._video_service = video_service
+        self.score = 0 # for the computation of score
         
     def start_game(self, cast):
         """Starts the game using the given cast. Runs the main game loop.
-
         Args:
             cast (Cast): The cast of actors.
         """
+
         self._video_service.open_window()
         while self._video_service.is_window_open():
             self._get_inputs(cast)
@@ -47,19 +47,47 @@ class Director:
         Args:
             cast (Cast): The cast of actors.
         """
+
         banner = cast.get_first_actor("banners")
+        banner2 = cast.get_first_actor("banners2") # added
         robot = cast.get_first_actor("robots")
         artifacts = cast.get_actors("artifacts")
 
-        banner.set_text("")
+        banner.set_text("Score: "+str(self.score))
         max_x = self._video_service.get_width()
         max_y = self._video_service.get_height()
         robot.move_next(max_x, max_y)
         
         for artifact in artifacts:
+            artifact.move_next(max_x, max_y)
             if robot.get_position().equals(artifact.get_position()):
-                message = artifact.get_message()
-                banner.set_text(message)    
+                # conditions if gems(*) are hit. with corresponding scores based on font size
+                if artifact.get_text() == '*' and artifact.get_font_size() == 15:
+                    self.score += 1
+                    artifact.set_text("")
+                elif artifact.get_text() == '*' and artifact.get_font_size() == 20:
+                    self.score += 3
+                    artifact.set_text("")
+                elif artifact.get_text() == '*' and artifact.get_font_size() == 25:
+                    self.score += 5
+                    artifact.set_text("")
+                # else condition when rocks(O) are hit with corresponding deduction of score based on font size
+                else:
+                    if self.score == 0:
+                        self.score = 0
+                        artifact.set_text("")
+                    else:
+                        if artifact.get_font_size() == 15:
+                            self.score -= 1
+                            artifact.set_text("")
+                        elif artifact.get_font_size() == 20:
+                            self.score -= 3
+                            artifact.set_text("")
+                        elif artifact.get_font_size() == 25:
+                            self.score -= 5
+                            artifact.set_text("")
+                # message = artifact.get_message()
+            banner2.set_text(chr(self.score))
         
     def _do_outputs(self, cast):
         """Draws the actors on the screen.
