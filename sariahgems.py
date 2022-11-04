83fda8ec46d05d9e687b42e3497b8d5ed08d1e4d
montygal <98441725+montygal@users.noreply.github.com>
2022-11-03 19:43:04 -0600

    Add files via upload

---
+++ sariahgems.py
@@ -0,0 +1,71 @@
+#Import libraries
+import pygame
+import random 
+r = random.randint(0,255) 
+g = random.randint(0,255) 
+b = random.randint(0,255) 
+rgb = [r,g,b] 
+print('A Random RGB Value :',rgb)
+#Run the main loop
+self_is_playing = True
+
+#Number of gems to start out with
+num_gems = 50
+
+#Set the variable 'person' to x
+person = 'x'
+
+#Set window size
+size = (900, 600)
+screen = pygame.display.set_mode(size)
+
+#Have an open_window class to set dimensions for the window
+class open_window:
+    def __init__(size):
+        if self_is_playing:
+            print(size)
+        return    
+
+#Have an open class to call the open_window to draw the board and print the player(x)
+class open:
+    def __init__(open_window, person):
+        print(person)
+        return
+
+#print the screen for pygame
+print(screen)
+
+#Create 50 gems and scatter them accross the screen
+class cell:
+    def __init__(x_axis, y_axis):
+        for i in range(len(50)):
+            x_axis = (50, 150)
+            y_axis = (100, 200)
+            print(x_axis, y_axis)
+        return
+
+#Call the cells and give them shapes
+class cell_size:
+    def __init__(cell, circle_gem, star_gem):
+        circle_gem = 'o'
+        star_gem = '*'
+        print(cell, circle_gem, star_gem)
+        return
+
+#Give the shapes a color by calling them from the cell_size class
+class gem_color:
+    def __init__(cell_size, rgb):
+        rgb = [r,g,b] 
+        print(cell_size,'A Random RGB Value :',rgb)
+        return    
+
+#Create a class to clear the screen and quit pygame
+class clear_buffer:
+    def __init__(self_is_playing):
+        if self_is_playing == False:
+            quit
+        return
+
+#Wait until everything is drawn to start playing
+pygame.display.flip()
+
